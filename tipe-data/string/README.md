7c09be0342d48cc2c0f5a9258a0dfd7aa83c1b51
andidev30 <andidev30.personal@gmail.com>
2021-12-28 21:41:53 +0700

    tipe data string

---
+++ tipe-data/string/README.md
@@ -0,0 +1,10 @@
+# go-learning
+
+## tipe data string
+
+- string adalah kumpulan data
+- string menggunakan petik dua diawal dan akhir
+- menggunakan kata kunci string
+
+- function untuk string
+  ![image info](./pictures/string.png)
---
+++ tipe-data/string/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("nama")
+
+	// function pada string
+	// ====================
+	// len untuk menghitung jumlah karakter
+	fmt.Println(len("nama")) // result 4
+	// string[number] untuk mengambil karakterk ke-
+	fmt.Print("andi"[0]) // result 97 (belum di konversi)
+}
